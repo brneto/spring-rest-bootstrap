30ef85a5e1ecc40e37532ede3e022527c0e28f61
Berchris Requiao <brneto@gmail.com>
2020-11-17 19:14:12 +0000

    Update spring boot version

--- src/main/java/ie/ait/ria/springrestbootstrap/SpringRestBootstrapApplication.java
+++ src/main/java/ie/ait/ria/springrestbootstrap/SpringRestBootstrapApplication.java
@@ -9,11 +9,13 @@ import org.springframework.beans.factory.InjectionPoint;
 import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Scope;
 import org.springframework.core.MethodParameter;
 
 @SpringBootApplication
+@ConfigurationPropertiesScan
 public class SpringRestBootstrapApplication {
 
   public static void main(String[] args) {
